5a1f9d7567cc4ddbf94fa159df642c66ec30390b
Jaime Perdiguero Alonso <jperdiguero03@gmail.com>
2025-01-28 14:49:59 +0100

    Update UsuarioRegistrado.cpp

--- CARPETA/UsuarioRegistrado.cpp
+++ CARPETA/UsuarioRegistrado.cpp
@@ -2,7 +2,7 @@
 //  UsuarioRegistrado.cpp
 //  PRÁCTICA SISTEMA BANCARIO
 //
-//  Created by Eduardo Ros on 7/11/23.
+//  Created by Eduardo Ros and Jaime Perdiguero on 7/11/23.
 //
 
 #include "UsuarioRegistrado.hpp"
