5cc0e084a8e4bb2d8f1398aee8f6c8672277e267
Jaime Perdiguero Alonso <jperdiguero03@gmail.com>
2025-01-28 14:50:13 +0100

    Update UsuarioRegistrado.hpp

--- CARPETA/UsuarioRegistrado.hpp
+++ CARPETA/UsuarioRegistrado.hpp
@@ -2,7 +2,7 @@
 //  UsuarioRegistrado.hpp
 //  PRÁCTICA SISTEMA BANCARIO
 //
-//  Created by Eduardo Ros on 7/11/23.
+//  Created by Eduardo Ros and Jaime Perdiguero on 7/11/23.
 //
 
 #ifndef UsuarioRegistrado_hpp
