47b1b82ea8177e9cc2cf3da63dff41867362127b
Jaime Perdiguero Alonso <jperdiguero03@gmail.com>
2025-01-28 14:48:33 +0100

    Update Usuario.cpp

--- CARPETA/Usuario.cpp
+++ CARPETA/Usuario.cpp
@@ -2,7 +2,7 @@
 //  class Usuario.cpp
 //  PRÁCTICA SISTEMA BANCARIO
 //
-//  Created by Eduardo Ros on 7/11/23.
+//  Created by Eduardo Ros and Jaime Perdiguero on 7/11/23.
 //
 
 #include "Usuario.hpp"
