83e846bf4c32ef23156d70d437176366865d5b4f
Jaime Perdiguero Alonso <jperdiguero03@gmail.com>
2025-01-28 14:50:28 +0100

    Update main.cpp

--- CARPETA/main.cpp
+++ CARPETA/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  PRÁCTICA SISTEMA BANCARIO
 //
-//  Created by Eduardo Ros on 16/10/23.
+//  Created by Eduardo Ros and Jaime Perdiguero on 16/10/23.
 //
 #include <fstream>
 #include <string>
